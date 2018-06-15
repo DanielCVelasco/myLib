42dcc3257c488af4a5672ca37b4acc342716f0c0
admin <admin@iMac-de-admin-4.local>
2018-06-15 12:29:51 -0500

    First Commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/Car.java
@@ -0,0 +1,17 @@
+package com.example.mylibrary;
+
+public class Car {
+    private int numberDors;
+    private int numberOfCylindres;
+    private int currentSpeed;
+
+    public Car(){
+
+    }
+
+    public Car(int numberDors, int numberOfCylindres, int currentSpeed) {
+        this.numberDors = numberDors;
+        this.numberOfCylindres = numberOfCylindres;
+        this.currentSpeed = currentSpeed;
+    }
+}
